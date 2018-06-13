e1b2ea5a425e26444d3c8d7a440d35ab33cc5bad
Kevin Jolley <serrowpls@gmail.com>
2018-06-13 09:56:04 -0500

    update

---
+++ Code/Challenge Code/circleC.C
@@ -0,0 +1,78 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "math.h"
+
+typedef struct Center {
+  float x;
+  float y;
+} Center;
+
+typedef struct Circle {
+  Center* center;
+  float radius;
+} Circle;
+
+Circle *createCircle(float x, float y, float radius)
+{
+  Circle* c = malloc(sizeof(struct Circle));
+  c->center = malloc(sizeof(struct Center));
+  c->center->x = x;
+  c->center->y = y;
+  c->radius = radius;
+}
+
+
+int checkIntersection(Circle *c1, Circle *c2)
+{
+  float sq_x = pow(c1->center->x - c2->center->x, 2);
+  float sq_y = pow(c1->center->y - c2->center->y, 2);
+  
+  float sq_distance = sqrt(sq_x + sq_y);
+  float sq_radius = c1->radius + c2->radius;
+  
+  if (sq_distance == sq_radius) {
+    return 1;
+  }
+  else if (sq_distance > sq_radius) {
+    return -1;
+  }
+  else {
+    return 0;
+  }
+  
+  
+}
+
+
+// Helper function for testing; do not edit
+void printResult(int rv)
+{
+  if (rv == 1) {
+    printf("Circles lie tangent to each other\n");
+  } 
+  else if (rv < 0) {
+    printf("Circles do not intersect\n");
+  }
+  else {
+    printf("Circles intersect\n");
+  }
+}
+
+int main(void)
+{
+  Circle *c1 = createCircle(-10, 8, 30);
+  Circle *c2 = createCircle(14, -24, 10);
+  
+  int rv = checkIntersection(c1, c2);
+  
+  printResult(rv);
+  
+  c1 = createCircle(53.2329, 97.3497, 22.2222);
+  c2 = createCircle(29.3452, 28.0989, 51.3451);
+  
+  rv = checkIntersection(c1, c2);
+  
+  printResult(rv);
+  
+  return 0;
+}
